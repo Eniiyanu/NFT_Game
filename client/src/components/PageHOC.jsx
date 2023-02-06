03eed4adfb70d001ff95d5e4194b400ca2ca4ff2
eniiyanu <depopf89@gmail.com>
2023-02-06 19:01:16 +0100

    Updated PageHOC file

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import {logo, heroImg} from "../assets";
-import styles  from '../styles';
+import { logo, heroImg } from "../assets";
+import styles from "../styles";
 const PageHOC = () => {
   const navigate = useNavigate();
 
-    return (
-    <div className={styles.hocContainer}>PagHOC</div>
-  )
-}
+  return (
+    <div className={styles.hocContainer}>
+      <div className={styles.hocContainer}>
+        <img
+          src="{logo}"
+          alt="logo"
+          className="{styles.hoclogo}"
+          onClick={() => navigate("/")}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default PageHOC
+export default PageHOC;
