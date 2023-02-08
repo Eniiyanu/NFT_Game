9aab7b97c80e33ca1eac297c9f0cd13a61256eab
eniiyanu <depopf89@gmail.com>
2023-02-08 22:35:48 +0100

    update main.jsx

---
+++ client/src/page/CreateBattle.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { PageHOC } from "../components";
+const CreateBattle = () => {
+  return (
+    <div>
+      <h1 className="text-5xl p-3"></h1>
+     
+    </div>
+  );
+};
+
+export default PageHOC(
+    CreateBattle,
+  <>
+    Create <br /> a new battle
+  </>,
+  <>
+    Create your own battle and enjoy the game. <br />
+    The game is built with React, Solidity, and Avalanche's C-Chain.
+  </>
+);
