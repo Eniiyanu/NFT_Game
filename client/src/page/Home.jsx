1c7e6f6125b2d2508bef26ccf1231aba45f9bc86
eniiyanu <depopf89@gmail.com>
2023-02-08 22:25:56 +0100

    update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -3,9 +3,8 @@ import { PageHOC } from "../components";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by Oluwaferanmi Cyber Prof</p>
+      <h1 className="text-5xl p-3"></h1>
+     
     </div>
   );
 };
