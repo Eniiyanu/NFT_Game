7f77c7edb14161e4c457ebe6d0a479d716f6c23a
eniiyanu <depopf89@gmail.com>
2023-02-08 22:09:37 +0100

    update PageHOC.jsx

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -17,7 +17,7 @@ const PageHOC = (Component, title, description) => () => {
         />
         <div className={styles.hocBodyWrapper}>
           <div className="flex flex-row w-full"> 
-          <h1 className= {'flex ${styles.headText} head-text'} >{title}</h1>
+          <h1 className= {'flex ${styles.headText} head-text'}>{title}</h1>
           </div>
           <p>{description}</p>
 
