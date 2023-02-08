e2a6eb318f903aaf987d4c9039410da4a2b2b0fb
eniiyanu <depopf89@gmail.com>
2023-02-08 22:23:58 +0100

    update PageHOC.jsx

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -1,31 +1,39 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { logo, heroImg } from "../assets";
-import styles from "../styles";
+//import Alert from './Alert';
+//import { useGlobalContext } from '../context';
+import { logo, heroImg } from '../assets';
+import styles from '../styles';
+
 const PageHOC = (Component, title, description) => () => {
+  //const { showAlert } = useGlobalContext();
   const navigate = useNavigate();
 
   return (
     <div className={styles.hocContainer}>
+
       <div className={styles.hocContentBox}>
-        <img
-          src={logo}
-          alt="logo"
-          className={styles.hocLogo}
-          onClick={() => navigate("/")}
-        />
+        <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => navigate('/')} />
+
         <div className={styles.hocBodyWrapper}>
-          <div className="flex flex-row w-full"> 
-          <h1 className= {'flex ${styles.headText} head-text'}>{title}</h1>
+          <div className="flex flex-row w-full">
+            <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
           </div>
-          <p className={'${styles.normalText} my-10'}>{description}</p>
+
+          <p className={`${styles.normalText} my-10`}>{description}</p>
 
           <Component />
         </div>
+
+        <p className={styles.footerText}>Made with 💜 by Oluw</p>
+      </div>
+
+      <div className="flex flex-1">
+        <img src={heroImg} alt="hero-img" className="w-full xl:h-full object-cover" />
       </div>
     </div>
   );
 };
 
-export default PageHOC
+export default PageHOC;
