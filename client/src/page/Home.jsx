57b023278fa8c0d2b0f4f945a39712d50e7fc320
eniiyanu <depopf89@gmail.com>
2023-02-08 22:15:32 +0100

    update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { PageHOC } from '../components';
+import React from "react";
+import { PageHOC } from "../components";
 const Home = () => {
   return (
     <div>
@@ -7,14 +7,16 @@ const Home = () => {
       <h2 className="text-3xl p-3">NFT Battle-style Card Game</h2>
       <p className="text-xl p-3">Made with 💜 by Oluwaferanmi Cyber Prof</p>
     </div>
-  )
+  );
 };
 
-export default PageHOC( 
-Home,
-<>Welcome to Avax Gods <br /> A Web3 Card game</>,
-<>A battle-style card game built on Avalanche's C-Chain. <br />
-
-The game is built with React, Solidity, and Avalanche's C-Chain. </>
-
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Avax Gods <br /> A Web3 Card game
+  </>,
+  <>
+    A battle-style card game built on Avalanche's C-Chain. <br />
+    The game is built with React, Solidity, and Avalanche's C-Chain.
+  </>
 );
