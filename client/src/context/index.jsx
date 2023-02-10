b5cfe0d442e48d7eaa0122e02a989e2d9bdb5531
eniiyanu <depopf89@gmail.com>
2023-02-10 22:46:40 +0100

    update client/src/context/index.jsx

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,4 +1,16 @@
-import React, {createContext, useContext, useEffect, useRef, useState} from 'react';
-import {ethers} from 'ethers';
-import Web3Modal from 'web3modal';
-import { useNavigate } from 'react-router-dom';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { useNavigate } from "react-router-dom";
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+  return <GlobalContext.Provider value={{}}></GlobalContext.Provider>;
+};
