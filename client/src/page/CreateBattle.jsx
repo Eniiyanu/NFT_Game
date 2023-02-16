0a947456d60c2db3dd1c0f06623220aa5f565874
eniiyanu <depopf89@gmail.com>
2023-02-16 16:45:42 +0100

    update CreateBattle.jsx update Home.jsx

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -17,6 +17,6 @@ export default PageHOC(
   <>
     Create your own battle and enjoy the game. <br />
     The game is built with React, Solidity, and Avalanche's C-Chain.
-    Let's Game!!!
+    Let's us Game!!!
   </>
 );
