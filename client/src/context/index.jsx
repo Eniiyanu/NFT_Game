5e1ddeb8973c0f86515e3c35cbf88248203377c6
eniiyanu <depopf89@gmail.com>
2023-02-17 23:24:40 +0100

    update client/src/context/index.jsx

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -12,24 +12,20 @@ import { useNavigate } from "react-router-dom";
 const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
-    const {walletAddress, setWalletAddress} = useState(null);
-    const updateCurrentWalletAddress = async () => {
-        const accounts = await window.ethereum.request({
-            method: "eth_requestAccounts",
-        });
-    }
+  const { walletAddress, setWalletAddress } = useState(null);
+  const updateCurrentWalletAddress = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+  console.log(accounts);
+  };
+  useEffect(() => {
+    updateCurrentWalletAddress();
+  }, []);
 
 
 
-
-
-  return <GlobalContext.Provider value={{
-
-
-
-  }}>
-    {children}
-  </GlobalContext.Provider>;
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
 };
 
 export const useGlobalContext = () => useContext(GlobalContext);
