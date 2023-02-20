4b902a732c21044d991f39940603418875af5ca5
eniiyanu <depopf89@gmail.com>
2023-02-20 20:57:06 +0100

    update client/src/contract/index.js

--- client/src/contract/index.js
+++ client/src/contract/index.js
@@ -1,1 +1,5 @@
-export const ADDRESS = "0xe4ea1075a5915F6d744388503c05a00817058456"
+import contract from "./AVAXGods.json";
+
+export const ADDRESS = "0xe4ea1075a5915F6d744388503c05a00817058456";
+
+export const { abi: ABI } = contract;
