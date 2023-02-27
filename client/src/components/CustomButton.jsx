0e91b67d553f958cbf7d3ddd0974813e04d27c38
eniiyanu <depopf89@gmail.com>
2023-02-27 11:02:20 +0100

    update CustomButton.jsx

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -4,11 +4,12 @@ import styles from '../styles'
 const CustomButton = ({title, handleClick,restStyles}) => {
   return (
     
-    <button>
+    <button
         type ="button"
         className ={ `${styles.btn} ${restStyles} `}
         onClick = {handleClick}
-        {title}
+        >
+{title}
     </button>
   )
 }
