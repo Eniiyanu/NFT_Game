a646a1cc3111dca573fc185c2ca16023d23b084c
eniiyanu <depopf89@gmail.com>
2023-02-27 10:46:46 +0100

    update CustomInput.jsx

--- client/src/components/CustomInput.jsx
+++ client/src/components/CustomInput.jsx
@@ -8,14 +8,14 @@ const CustomInput = ({label, placeholder, value, handleValueChange}) => {
     <>
     <label htmlfor ='name' className={styles.label}> {label} </label>
 
-    <input type ="text"  placeholder={placeholder} value={value} onChange={(e)} =>{
-      if (e.target.value === '' || regex.test(e.target.value)) {
-    }
+    <input type ="text"  placeholder={placeholder} value={value} onChange={(e) => {
+      if (e.target.value === '' || regex.test(e.target.value)) handleValueChange(e.target.value)
+    
 
 
 
-
-     />
+    }} className={styles.input} />
+     
   
 </>
 )
