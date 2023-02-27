da4f4e91783f52caf0adf058f9b603b794cfec38
eniiyanu <depopf89@gmail.com>
2023-02-27 10:44:42 +0100

    update CustomInput.jsx

--- client/src/components/CustomInput.jsx
+++ client/src/components/CustomInput.jsx
@@ -5,8 +5,18 @@ const regex = /^[a-zA-Z0-9]+$/
 
 const CustomInput = ({label, placeholder, value, handleValueChange}) => {
   return (
-    <label> html </label>
-  )
-}
+    <>
+    <label htmlfor ='name' className={styles.label}> {label} </label>
 
+    <input type ="text"  placeholder={placeholder} value={value} onChange={(e)} =>{
+      if (e.target.value === '' || regex.test(e.target.value)) {
+    }
+
+
+
+
+     />
+  
+</>
+)
 export default CustomInput
